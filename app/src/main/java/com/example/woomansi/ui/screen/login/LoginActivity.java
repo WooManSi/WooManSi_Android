b7a1313f552ba5626a4108740a46136f71439ade
leejunha <jjuna0502@gmail.com>
2022-11-09 21:30:45 +0900

    test

--- app/src/main/java/com/example/woomansi/ui/screen/login/LoginActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/login/LoginActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.woomansi.R;
@@ -19,23 +19,57 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView join;
     private Button login;
-    private EditText email_login;
-    private EditText pwd_login;
-    FirebaseAuth firebaseAuth;
+    private EditText email_login, pwd_login;
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        login = findViewById(R.id.LoginActivity_btn_login);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("woomansi");
+        email_login = findViewById(R.id.LoginActivity_et_Id_Text);
+        pwd_login = findViewById(R.id.LoginActivity_et_Pw_Text);
 
-        TextView tw_goto_register = findViewById(R.id.LoginActivity_tv_goto_signup);
-        tw_goto_register.setOnClickListener(new View.OnClickListener() {
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //로그인 요청
+                String strEmail = email_login.getText().toString();        //문자열로 변환된 입력값을 변수안에 저장
+                String strPwd = pwd_login.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            //로그인성공
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish(); //현재 액티비티 파괴
+                        }
+                        else {
+                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+        join = findViewById(R.id.LoginActivity_tv_goto_signup);
+        join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //회원가입 화면으로 이동
