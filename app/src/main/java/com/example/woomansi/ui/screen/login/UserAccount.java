fd08fa9894fc387d962b18101d8dccfbdf8e6d5a
leejunha <jjuna0502@gmail.com>
2022-11-10 15:32:41 +0900

    add splashactivity

--- app/src/main/java/com/example/woomansi/ui/screen/login/UserAccount.java
+++ app/src/main/java/com/example/woomansi/ui/screen/login/UserAccount.java
@@ -9,6 +9,7 @@ public class UserAccount {
     private String emailId;         //  이메일 아이디
     private String password;        // 비밀번호
 
+
     public UserAccount() {    }         //파이어베이스 realtime database 를 사용할때 빈 생성자를 만들어 줘야해서 추가.
 
 //getter and setter 세팅
