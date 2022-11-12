8e5121e055b825401c87572961f1c04d9d64f354
EunHee-21 <heh7878@gmail.com>
2022-11-13 07:34:25 +0900

    feat: 그룹 정보화면 - 그룹명과 비밀번호를 받아오는 기능 작성 완료

--- app/src/main/java/com/example/woomansi/ui/screen/group/GroupInfoActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/group/GroupInfoActivity.java
@@ -7,12 +7,15 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.woomansi.R;
 import com.example.woomansi.ui.screen.main.MainActivity;
 
 public class GroupInfoActivity extends AppCompatActivity {
 
+    private ImageButton backBtn;
     private Button exitGroupBtn;
     private Dialog dialog_exitGroup;
 
@@ -21,8 +24,17 @@ public class GroupInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_info);
 
+        //뒤로가기 화살표 버튼
+        backBtn = findViewById(R.id.groupInfo_ib_backButton);
+        backBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         //그룹 나가기 버튼
-        exitGroupBtn = findViewById(R.id.groupInfo_exitGroupButton);
+        exitGroupBtn = findViewById(R.id.groupInfo_btn_exitGroupButton);
         exitGroupBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -35,13 +47,19 @@ public class GroupInfoActivity extends AppCompatActivity {
         dialog_exitGroup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
         //TODO: 그룹명하고 비밀번호 서버에서 가져와서 띄워주기
+        TextView groupName = findViewById(R.id.groupInfo_tv_groupName);
+        groupName.setText(getIntent().getStringExtra("그룹명"));
+
+        TextView password = findViewById(R.id.groupInfo_tv_Password);
+        password.setText(getIntent().getStringExtra("비밀번호"));
+
     }
 
     public void showExitGroupDialog(){
         dialog_exitGroup.show(); // 다이얼로그 띄우기
 
         //취소 버튼
-        Button cancelBtn = dialog_exitGroup.findViewById(R.id.dialog_exitGroup_cancelButton);
+        Button cancelBtn = dialog_exitGroup.findViewById(R.id.dialog_exitGroup_btn_cancel);
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,12 +68,13 @@ public class GroupInfoActivity extends AppCompatActivity {
         });
 
         //그룹탈퇴 버튼
-        Button joinBtn = dialog_exitGroup.findViewById(R.id.dialog_exitGroup_acceptExitButton);
+        Button joinBtn = dialog_exitGroup.findViewById(R.id.dialog_exitGroup_btn_acceptExit);
         joinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //TODO: 누르면 해당 사용자는 그룹에서 탈퇴되며, 사용자의 그룹리스트에서 해당그룹이 삭제됨.
-                finish();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
     }
