24f95959280da884c530d14b4fc6c143e6beec87
EunHee-21 <heh7878@gmail.com>
2022-11-13 22:10:57 +0900

    feat: 그룹 탈퇴로직 작성

--- app/src/main/java/com/example/woomansi/ui/screen/group/GroupInfoActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/group/GroupInfoActivity.java
@@ -9,20 +9,39 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.woomansi.R;
+import com.example.woomansi.data.model.GroupModel;
 import com.example.woomansi.ui.screen.main.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import java.util.ArrayList;
 
 public class GroupInfoActivity extends AppCompatActivity {
 
     private ImageButton backBtn;
     private Button exitGroupBtn;
     private Dialog dialog_exitGroup;
+    private TextView groupName;
+    private TextView password;
+    private GroupModel group;
+
+    private FirebaseAuth auth;
+    private FirebaseFirestore fireStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_info);
+
+        auth = FirebaseAuth.getInstance();
+        fireStore = FirebaseFirestore.getInstance();
 
         //뒤로가기 화살표 버튼
         backBtn = findViewById(R.id.groupInfo_ib_backButton);
@@ -46,13 +65,13 @@ public class GroupInfoActivity extends AppCompatActivity {
         dialog_exitGroup.setContentView(R.layout.dialog_exit_group);
         dialog_exitGroup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-        //TODO: 그룹명하고 비밀번호 서버에서 가져와서 띄워주기
-        TextView groupName = findViewById(R.id.groupInfo_tv_groupName);
-        groupName.setText(getIntent().getStringExtra("그룹명"));
+        group = (GroupModel) getIntent().getSerializableExtra("group");
 
-        TextView password = findViewById(R.id.groupInfo_tv_Password);
-        password.setText(getIntent().getStringExtra("비밀번호"));
+        groupName = findViewById(R.id.groupInfo_tv_groupName);
+        groupName.setText(group.getGroupName());
 
+        password = findViewById(R.id.groupInfo_tv_Password);
+        password.setText(group.getGroupPassword());
     }
 
     public void showExitGroupDialog(){
@@ -72,9 +91,54 @@ public class GroupInfoActivity extends AppCompatActivity {
         joinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: 누르면 해당 사용자는 그룹에서 탈퇴되며, 사용자의 그룹리스트에서 해당그룹이 삭제됨.
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+                ArrayList<String> memberList = group.getMemberList();
+                //멤버리스트에서 리더 Uid를 빼주는 데이터 처리 작업
+                String currentUserUid = auth.getCurrentUser().getUid();
+                memberList.remove(currentUserUid);
+
+                //만약 그룹탈퇴를 요청한 사용자가 리더일 경우, 그룹 자체를 삭제
+                if(currentUserUid.equals(group.getLeaderUid())){
+                    fireStore.collection("groups")
+                        .whereEqualTo("groupName", group.getGroupName())
+                        .whereEqualTo("groupPassword", group.getGroupPassword())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (DocumentSnapshot document : task.getResult()) {
+                                        DocumentReference d = document.getReference();
+                                        System.out.println(d);
+                                        d.delete();
+                                    }
+                                }
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                intent.putExtra("group", "group");
+                                startActivity(intent);
+                            }
+                        });
+                    //만약 그룹탈퇴를 요청한 사용자가 그룹원일 경우, 해당 그룹원을 멤버 리스트에서 제외
+                } else {
+                    fireStore.collection("groups")
+                        .whereEqualTo("groupName", group.getGroupName())
+                        .whereEqualTo("groupPassword", group.getGroupPassword())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (DocumentSnapshot document : task.getResult()) {
+                                        DocumentReference d = document.getReference();
+                                        System.out.println(d);
+                                        d.update("memberList", memberList);
+                                    }
+                                }
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                intent.putExtra("group", group);
+                                startActivity(intent);
+                            }
+                        });
+                }
             }
         });
     }
