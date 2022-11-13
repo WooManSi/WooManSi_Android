b4cdf3904a0bfc41a219511de07feabf78dc17dc
EunHee-21 <heh7878@gmail.com>
2022-11-13 22:11:04 +0900

    feat: 그룹 생성로직 작성

--- app/src/main/java/com/example/woomansi/ui/screen/group/GroupCreateActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/group/GroupCreateActivity.java
@@ -1,18 +1,32 @@
 package com.example.woomansi.ui.screen.group;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import android.widget.Toast;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.woomansi.R;
 import com.example.woomansi.data.model.GroupModel;
+import com.example.woomansi.ui.screen.main.MainActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -28,7 +42,6 @@ public class GroupCreateActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseFirestore fireStore;
 
-    //onCreate 함수
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,31 +67,59 @@ public class GroupCreateActivity extends AppCompatActivity {
         createGroupBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                /*TODO: 그룹명, 비밀번호 들어왔는지 모두 검사하고,
-                   정상이면 서버에 그룹데이터 넣고 사용자 그룹리스트에 그룹 추가해줌.
-                 */
                 String groupName = editGroupName.getText().toString();
                 String groupPassword = editGroupPassword.getText().toString();
 
-                GroupModel group = new GroupModel(groupName, groupPassword, getTime());
+                GroupModel group = new GroupModel();
+                group.setGroupName(groupName);
+                group.setGroupPassword(groupPassword);
+                group.setGroupCreateDate(getTime());
 
-                System.out.println("그룹이름 : " + group.getGroupName() + ", 그룹생성날짜 : " + group.getGroupCreateDate());
-/*                fireStore.collection("groups")
-                        .add(group)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w(TAG, "Error adding document", e);
-                            }
-                        });*/
+                String currentUserUid = auth.getCurrentUser().getUid();
+                group.setLeaderUid(currentUserUid);
 
-                finish();
+                ArrayList<String> memberList = new ArrayList<>();
+                memberList.add(currentUserUid);
+                group.setMemberList(memberList);
+
+                fireStore.collection("groups")
+                    .whereEqualTo("groupName", groupName)
+                    .whereEqualTo("groupPassword", groupPassword)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                //그룹명과 비밀번호가 둘 다 똑같으면 그룹생성 불가.
+                                if(!task.getResult().isEmpty()) {
+                                    Toast.makeText(GroupCreateActivity.this, "그룹명과 비밀번호가 모두 일치하는 동일한 그룹이 이미 존재합니다.", Toast.LENGTH_SHORT).show();
+                                }
+                                else {
+                                    fireStore.collection("groups")
+                                        .add(group)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                Log.d(TAG, "그룹이름 : " + group.getGroupName() + ", 그룹생성날짜 : " + group.getGroupCreateDate());
+                                                Toast.makeText(GroupCreateActivity.this, "그룹생성 완료!", Toast.LENGTH_SHORT).show();
+
+                                                //finish()로 연결할 시 에러가 나서, intent로 새 화면 연결해줌.
+                                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                                intent.putExtra("group", "group");
+                                                startActivity(intent);
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "그룹 추가 과정에서 에러 발생", e);
+                                                Toast.makeText(GroupCreateActivity.this, "그룹생성 실패", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                }
+                            }
+                        }
+                    });
             }
         });
     }
