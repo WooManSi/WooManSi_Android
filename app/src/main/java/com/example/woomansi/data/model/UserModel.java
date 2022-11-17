42e794f5f0316080eaaf502a835c5a4371d424dc
leejunha <jjuna0502@gmail.com>
2022-11-17 22:48:30 +0900

    feat : profile 액티비티 test

--- app/src/main/java/com/example/woomansi/data/model/UserModel.java
+++ app/src/main/java/com/example/woomansi/data/model/UserModel.java
@@ -1,5 +1,7 @@
 package com.example.woomansi.data.model;
 
+import java.net.URI;
+
 /**
  * 사용자 계정 정보 모델 클래스
  */
@@ -9,14 +11,16 @@ public class UserModel {
     private String emailId;         //  이메일 아이디
     private String password;        // 비밀번호
     private String nickname;
+    private URI profile;
 
     public UserModel() {    }         //파이어베이스 realtime database 를 사용할때 빈 생성자를 만들어 줘야해서 추가.
 
-    public UserModel(String idToken, String emailId, String password, String nickname) {
+    public UserModel(String idToken, String emailId, String password, String nickname, URI profile) {
         this.idToken = idToken;
         this.emailId = emailId;
         this.password = password;
         this.nickname = nickname;
+        this.profile = profile;
 
     }
 //getter and setter 세팅
@@ -35,4 +39,12 @@ public class UserModel {
     public String getNickname() {        return nickname;    }
 
     public void setNickname(String nickname) {        this.nickname = nickname;    }
+
+    public URI getProfile() {
+        return profile;
+    }
+
+    public void setProfile(URI profile) {
+        this.profile = profile;
+    }
 }
--- app/src/main/java/com/example/woomansi/ui/screen/login/ProfileActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/login/ProfileActivity.java
@@ -2,23 +2,125 @@ package com.example.woomansi.ui.screen.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 
+import com.bumptech.glide.Glide;
 import com.example.woomansi.R;
+import com.example.woomansi.ui.screen.main.Main1Fragment;
+import com.example.woomansi.ui.screen.main.MainActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ProfileActivity extends AppCompatActivity {
     private ImageView imageView;
-    private ProgressBar progressBar;
-    private final DatabaseReference root = FirebaseDatabase.getInstance().getReference("image");
+    private Button button;
+
+    private ImageView load;
+
+    private void customDialog() {
+        Dialog dialog = new Dialog(ProfileActivity.this);
+
+        dialog.setContentView(R.layout.fragment_avatar);
+        dialog.setTitle("custom dialog !!");
+        ImageView iv1 = dialog.findViewById(R.id.image1);
+        ImageView iv2 = dialog.findViewById(R.id.image2);
+        ImageView iv3 = dialog.findViewById(R.id.image3);
+        ImageView iv4 = dialog.findViewById(R.id.image4);
+        ImageView iv5 = dialog.findViewById(R.id.image5);
+        ImageView iv6 = dialog.findViewById(R.id.image6);
+        Button button = dialog.findViewById(R.id.btn_ok);
+        Glide.with(this).load(R.drawable.ic_profile1).into(iv1);
+        Glide.with(this).load(R.drawable.ic_profile2).into(iv2);
+        Glide.with(this).load(R.drawable.ic_profile3).into(iv3);
+        Glide.with(this).load(R.drawable.ic_profile4).into(iv4);
+        Glide.with(this).load(R.drawable.ic_profile5).into(iv5);
+        Glide.with(this).load(R.drawable.ic_profile6).into(iv6);
+        dialog.show();
+
+        iv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.ic_profile1);
+
+            }
+        });
+        iv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.ic_profile2);
+            }
+        });
+        iv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.ic_profile3);
+            }
+        });
+        iv4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.ic_profile4);
+            }
+        });
+        iv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.ic_profile5);
+            }
+        });
+        iv6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.ic_profile6);
+            }
+        });
 
 
+        button.setOnClickListener(view -> dialog.dismiss());
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        button = findViewById(R.id.ProfileActivity_bt_profilebtn);
+        imageView = findViewById(R.id.ProfileActivity_iv_image);
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customDialog();
+            }
+        });
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference();
+        StorageReference pathReference = storageReference.child("ic_profile6");
+        pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(ProfileActivity.this).load(uri).into(load);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
