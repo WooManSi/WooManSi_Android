2572e546094fe35c074acedded4a52b0ea890306
leejunha <jjuna0502@gmail.com>
2022-11-21 11:57:19 +0900

    feat : profile 액티비티 test2...

--- app/src/main/java/com/example/woomansi/ui/screen/login/ProfileActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/login/ProfileActivity.java
@@ -35,8 +35,8 @@ public class ProfileActivity extends AppCompatActivity {
     private ImageView imageView;
     private Button button;
     private ImageView[] mIv = new ImageView[6];
-    private int result;
-    private int n_result;
+    private int result = 0;
+    private int n_result = 0;
     private FirebaseStorage mStorage;
     private FirebaseDatabase mDatabase;
     private DatabaseReference mDatabaseRef;
@@ -46,65 +46,58 @@ public class ProfileActivity extends AppCompatActivity {
         Dialog dialog = new Dialog(ProfileActivity.this);
         dialog.setContentView(R.layout.fragment_avatar);
         dialog.setTitle("custom dialog !!");
-
+        ImageView iv1 = dialog.findViewById(R.id.image1);
+        ImageView iv2 = dialog.findViewById(R.id.image2);
+        ImageView iv3 = dialog.findViewById(R.id.image3);
+        ImageView iv4 = dialog.findViewById(R.id.image4);
+        ImageView iv5 = dialog.findViewById(R.id.image5);
+        ImageView iv6 = dialog.findViewById(R.id.image6);
         Button button = dialog.findViewById(R.id.btn_ok);
 
         dialog.show();
-
-        mIv[0] = (ImageView) findViewById(R.id.image1);
-        mIv[1] = (ImageView) findViewById(R.id.image2);
-        mIv[2] = (ImageView) findViewById(R.id.image3);
-        mIv[3] = (ImageView) findViewById(R.id.image4);
-        mIv[4] = (ImageView) findViewById(R.id.image5);
-        mIv[5] = (ImageView) findViewById(R.id.image6);
-
-
-        mIv[0].setOnClickListener(new View.OnClickListener() {
+        iv1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 imageView.setImageResource(R.drawable.ic_profile1);
                 result = 1;
             }
         });
-        mIv[1].setOnClickListener(new View.OnClickListener() {
+        iv2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 imageView.setImageResource(R.drawable.ic_profile2);
                 result = 2;
             }
         });
-        mIv[2].setOnClickListener(new View.OnClickListener() {
+        iv3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 imageView.setImageResource(R.drawable.ic_profile3);
                 result = 3;
             }
         });
-        mIv[3].setOnClickListener(new View.OnClickListener() {
+        iv4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 imageView.setImageResource(R.drawable.ic_profile4);
                 result = 4;
             }
         });
-        mIv[4].setOnClickListener(new View.OnClickListener() {
+        iv5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 imageView.setImageResource(R.drawable.ic_profile5);
                 result = 5;
             }
         });
-        mIv[5].setOnClickListener(new View.OnClickListener() {
+        iv6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 imageView.setImageResource(R.drawable.ic_profile6);
                 result = 6;
             }
         });
-
-
         button.setOnClickListener(view -> dialog.dismiss());
-
         return result;
     }
 
@@ -116,35 +109,38 @@ public class ProfileActivity extends AppCompatActivity {
 
         button = findViewById(R.id.ProfileActivity_bt_profilebtn);
         imageView = findViewById(R.id.ProfileActivity_iv_image);
-
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
 
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                n_result = customDialog();
-
+            public void onClick(View view)
+            {n_result = customDialog();}
+            });
 
                 button.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-                        FirebaseStorage storage = FirebaseStorage.getInstance();
+                        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+                        DatabaseReference conditionRef = mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).child("profile");
                         StorageReference storageReference = storage.getReference();
-                        StorageReference pathReference = storageReference.child("ic_profile" + n_result + ".png");
+                        StorageReference pathReference = storageReference.child("ic_profile"+n_result+".png");
                         pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                             @Override
                             public void onSuccess(Uri uri) {
                                 UserModel account = new UserModel();
-                                account.setProfile(uri.toString());
-                                mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                                String url = uri.toString();
+                                account.setProfile(url);
+                                conditionRef.setValue(account);
                             }
                         });
                         Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
                         startActivity(intent);
                     }
                 });
-
             }
-        });
-    }
-}
+        }
+
+
+
