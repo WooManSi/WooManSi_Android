cc361e69951066e94bda9512a58950347e9c8075
EunHee-21 <heh7878@gmail.com>
2022-11-22 04:16:42 +0900

    feat: 메인화면의 각 fragment에 TopAppBar 적용

--- app/src/main/java/com/example/woomansi/ui/screen/main/Main4Fragment.java
+++ app/src/main/java/com/example/woomansi/ui/screen/main/Main4Fragment.java
@@ -1,13 +1,18 @@
 package com.example.woomansi.ui.screen.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.woomansi.R;
+import com.example.woomansi.ui.screen.timetable.createTimeTableActivity;
 
 public class Main4Fragment extends Fragment {
 
@@ -22,8 +27,25 @@ public class Main4Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //appBar의 fragment개별 맞춤 설정
+        setHasOptionsMenu(true);
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main4, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_appbar_for_profile_change, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.appBar_deleteAccount) {
+            Intent intent = new Intent(getActivity().getApplicationContext(), createTimeTableActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
