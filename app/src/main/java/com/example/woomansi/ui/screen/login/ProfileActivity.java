512fb36ba6d1d17231f542f201c9cc593abcdd29
leejunha <jjuna0502@gmail.com>
2022-11-24 19:02:08 +0900

    refactor : PR part 수정

--- app/src/main/java/com/example/woomansi/ui/screen/login/ProfileActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/login/ProfileActivity.java
@@ -4,42 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.media.Image;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
 import com.example.woomansi.R;
-import com.example.woomansi.data.model.UserModel;
-import com.example.woomansi.ui.screen.main.Main1Fragment;
 import com.example.woomansi.ui.screen.main.MainActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
     private ImageView imageView;
     private Button button;
+    private int num = 0;
+    private int i = 0;
+    private int count = 0;
     private int result = 0;
     //private FirebaseStorage mStorage;
     //private FirebaseDatabase mDatabase;
@@ -49,61 +37,29 @@ public class ProfileActivity extends AppCompatActivity {
 
     private void customDialog() {
         Dialog dialog = new Dialog(ProfileActivity.this);
-        dialog.setContentView(R.layout.fragment_avatar);
+        dialog.setContentView(R.layout.dialog_profile);
         dialog.setTitle("custom dialog !!");
-        ImageView iv1 = dialog.findViewById(R.id.image1);
-        ImageView iv2 = dialog.findViewById(R.id.image2);
-        ImageView iv3 = dialog.findViewById(R.id.image3);
-        ImageView iv4 = dialog.findViewById(R.id.image4);
-        ImageView iv5 = dialog.findViewById(R.id.image5);
-        ImageView iv6 = dialog.findViewById(R.id.image6);
-        Button button_choose_iv = dialog.findViewById(R.id.btn_ok);
+        GridLayout g1 = dialog.findViewById(R.id.g_view);
+
+        count = g1.getChildCount();
 
         dialog.show();
-        iv1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageView.setImageResource(R.drawable.ic_profile1);
-                result = 1;
-            }
-        });
-        iv2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageView.setImageResource(R.drawable.ic_profile2);
-                result = 2;
-            }
-        });
-        iv3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageView.setImageResource(R.drawable.ic_profile3);
-                result = 3;
-            }
-        });
-        iv4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageView.setImageResource(R.drawable.ic_profile4);
-                result = 4;
-            }
-        });
-        iv5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageView.setImageResource(R.drawable.ic_profile5);
-                result = 5;
-            }
-        });
-        iv6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                imageView.setImageResource(R.drawable.ic_profile6);
-                result = 6;
-            }
-        });
-        button_choose_iv.setOnClickListener(view -> dialog.dismiss());
 
+        for(i = 0; i< count; i++) {
+            ImageView iv = (ImageView) g1.getChildAt(i);
+
+            StringBuilder fileName = new StringBuilder("ic_profile");
+            fileName.append(i+1);
+
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    imageView.setImageResource(getResources().getIdentifier(fileName.toString(), "drawable", getPackageName()));
+                    result = fileName.toString().compareTo("ic_profile0");
+                    dialog.dismiss();
+                }
+            });
+        }
     }
 
 
@@ -128,8 +84,6 @@ public class ProfileActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ProfileActivity.this, getString(R.string.example, result), Toast.LENGTH_SHORT).show();
-
                 DocumentReference profileRef = db.collection("users").document(firebaseUser.getUid());
                 profileRef
                         .update("profile", getString(R.string.profile,result))
@@ -146,19 +100,14 @@ public class ProfileActivity extends AppCompatActivity {
                 //DatabaseReference conditionRef = mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).child("profile");
                 /*StorageReference storageReference = storage.getReference();
                 StorageReference pathReference = storageReference.child(getString(R.string.profile,result));
-
                 pathReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                     @Override
                     public void onSuccess(Uri uri) {
                         Toast.makeText(ProfileActivity.this, "다운로드 성공", Toast.LENGTH_SHORT).show();
                         UserModel account = new UserModel();
                         String url = uri.toString();
-
                         Toast.makeText(ProfileActivity.this, url, Toast.LENGTH_SHORT).show();
-
                         UploadTask uploadTask=pathReference.putFile(uri);
-
-
                     }
                 });*/
                 Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
--- app/src/main/java/com/example/woomansi/ui/screen/main/Main4Fragment.java
+++ app/src/main/java/com/example/woomansi/ui/screen/main/Main4Fragment.java
@@ -9,21 +9,114 @@ import androidx.fragment.app.Fragment;
 
 import com.example.woomansi.R;
 
-public class Main4Fragment extends Fragment {
+import android.app.Dialog;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.woomansi.ui.screen.login.ProfileActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+
+public class Main4Fragment extends Fragment{
 
     public Main4Fragment() {
         // Required empty public constructor
     }
 
+
     public static Main4Fragment newInstance() {
         return new Main4Fragment();
     }
+
+    private ImageView imageView;
+    private Button button;
+    private int num = 0;
+    private int i = 0;
+    private int j = 0;
+    private int count = 0;
+    private int result = 0;
+    //private FirebaseStorage mStorage;
+    //private FirebaseDatabase mDatabase;
+    private FirebaseFirestore db;
+    private DatabaseReference mDatabaseRef;
+    private FirebaseAuth mFirebaseAuth;
+    private View view;
+    ArrayList idList = new ArrayList();
+
+    private void customDialog() {
+        Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_profile);
+        dialog.setTitle("custom dialog !!");
+        GridLayout g1 = dialog.findViewById(R.id.g_view);
+
+        count = g1.getChildCount();
+
+        dialog.show();
+
+        for(i = 0; i< count ; i++) {
+            ImageView iv = (ImageView) g1.getChildAt(i);
+            StringBuilder fileName = new StringBuilder("ic_profile");
+            fileName.append(i+1);
+
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    imageView.setImageResource(getResources().getIdentifier(fileName.toString(), "drawable", getActivity().getPackageName()));
+                    result = fileName.toString().compareTo("ic_profile0");
+                    dialog.dismiss();
+                }
+            });
+        }
+
+}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main4, container, false);
+        view = inflater.inflate(R.layout.fragment_main4, container, false);
+
+        db = FirebaseFirestore.getInstance();
+        button = view.findViewById(R.id.ProfileActivity_bt_profilebtn);
+        imageView = view.findViewById(R.id.ProfileActivity_iv_image);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+        //FirebaseStorage storage = FirebaseStorage.getInstance();
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              customDialog();
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DocumentReference profileRef = db.collection("users").document(firebaseUser.getUid());
+                profileRef
+                        .update("profile", getString(R.string.profile, result))
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(view.getContext(), "프로필 설정 완료", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        return view;
     }
+
 }
