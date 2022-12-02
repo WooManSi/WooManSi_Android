81fe87c48a1590057d7ca24a5fb59d4c8c88ab45
leejunha <jjuna0502@gmail.com>
2022-12-02 16:46:25 +0900

    feat : GroupVoteActivity XML 디자인

---
+++ app/src/main/java/com/example/woomansi/ui/screen/group/GroupVoteActivity.java
@@ -0,0 +1,16 @@
+package com.example.woomansi.ui.screen.group;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.woomansi.R;
+
+public class GroupVoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_vote);
+    }
+}
