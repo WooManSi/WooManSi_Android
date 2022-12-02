635268418a78920d9557b6bf6b11c6ba3838714b
eunhee21 <heh7878@gmail.com>
2022-12-02 17:10:47 +0900

    feat: 그룹 생성시 개인 시간표 -> 그룹 시간표로 변환하는 코드 작성

--- app/src/main/java/com/example/woomansi/ui/screen/group/GroupCreateActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/group/GroupCreateActivity.java
@@ -5,31 +5,29 @@ import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.woomansi.R;
 import com.example.woomansi.data.model.GroupModel;
+import com.example.woomansi.data.model.GroupTimeTableWrapper;
+import com.example.woomansi.data.repository.FirebaseSchedules;
 import com.example.woomansi.ui.screen.main.MainActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+import com.example.woomansi.util.CalculationUtil;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.TimeZone;
 
 
@@ -43,6 +41,8 @@ public class GroupCreateActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseFirestore fireStore;
 
+    List<String> dayNameList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,69 +51,68 @@ public class GroupCreateActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         fireStore = FirebaseFirestore.getInstance();
 
+        dayNameList = List.of(getResources().getStringArray(R.array.day_name));
+
         editGroupName = findViewById(R.id.groupCreate_et_editGroupName);
         editGroupPassword = findViewById(R.id.groupCreate_et_editGroupPassword);
 
 
         //그룹생성 버튼
         createGroupBtn = findViewById(R.id.groupCreate_btn_createGroup);
-        createGroupBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                String groupName = editGroupName.getText().toString();
-                String groupPassword = editGroupPassword.getText().toString();
+        createGroupBtn.setOnClickListener(v -> {
+            String groupName = editGroupName.getText().toString();
+            String groupPassword = editGroupPassword.getText().toString();
 
-                GroupModel group = new GroupModel();
-                group.setGroupName(groupName);
-                group.setGroupPassword(groupPassword);
-                group.setGroupCreateDate(getTime());
+            GroupModel group = new GroupModel();
+            group.setGroupName(groupName);
+            group.setGroupPassword(groupPassword);
+            group.setGroupCreateDate(getTime());
 
-                String currentUserUid = auth.getCurrentUser().getUid();
-                group.setLeaderUid(currentUserUid);
+            String currentUserUid = auth.getCurrentUser().getUid();
+            group.setLeaderUid(currentUserUid);
 
-                ArrayList<String> memberList = new ArrayList<>();
-                memberList.add(currentUserUid);
-                group.setMemberList(memberList);
+            ArrayList<String> memberList = new ArrayList<>();
+            memberList.add(currentUserUid);
+            group.setMemberList(memberList);
 
-                fireStore.collection("groups")
-                    .whereEqualTo("groupName", groupName)
-                    .whereEqualTo("groupPassword", groupPassword)
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if (task.isSuccessful()) {
-                                //그룹명과 비밀번호가 둘 다 똑같으면 그룹생성 불가.
-                                if(!task.getResult().isEmpty()) {
-                                    Toast.makeText(GroupCreateActivity.this, "그룹명과 비밀번호가 모두 일치하는 동일한 그룹이 이미 존재합니다.", Toast.LENGTH_SHORT).show();
-                                }
-                                else {
-                                    fireStore.collection("groups")
-                                        .add(group)
-                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                            @Override
-                                            public void onSuccess(DocumentReference documentReference) {
-                                                Log.d(TAG, "그룹이름 : " + group.getGroupName() + ", 그룹생성날짜 : " + group.getGroupCreateDate());
-                                                Toast.makeText(GroupCreateActivity.this, "그룹생성 완료!", Toast.LENGTH_SHORT).show();
+            GroupTimeTableWrapper timetable = new GroupTimeTableWrapper();
+            Map<String, List<Integer>> timetableMap = new HashMap<>();
 
-                                                //finish()로 연결할 시 에러가 나서, intent로 새 화면 연결해줌.
-                                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                                intent.putExtra("group", "group");
-                                                startActivity(intent);
-                                            }
-                                        })
-                                        .addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                Log.w(TAG, "그룹 추가 과정에서 에러 발생", e);
-                                                Toast.makeText(GroupCreateActivity.this, "그룹생성 실패", Toast.LENGTH_SHORT).show();
-                                            }
-                                        });
-                                }
-                            }
+            calculateGroupTimeTable(timetableMap);
+            timetable.setGroupTimeTable(timetableMap);
+
+
+            fireStore.collection("groups")
+                .whereEqualTo("groupName", groupName)
+                .whereEqualTo("groupPassword", groupPassword)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        //그룹명과 비밀번호가 둘 다 똑같으면 그룹생성 불가.
+                        if (!task.getResult().isEmpty()) {
+                            Toast.makeText(GroupCreateActivity.this, "그룹명과 비밀번호가 모두 일치하는 동일한 그룹이 이미 존재합니다.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            fireStore.collection("groups")
+                                    .add(group)
+                                    .addOnSuccessListener(documentReference -> {
+                                        String[] path = documentReference.getPath().split("/");
+                                        addGroupTimeTable(path[1], timetable);
+
+                                        Log.d(TAG, "그룹이름 : " + group.getGroupName() + ", 그룹생성날짜 : " + group.getGroupCreateDate());
+                                        Toast.makeText(GroupCreateActivity.this, "그룹생성 완료!", Toast.LENGTH_SHORT).show();
+
+                                        //finish()로 연결할 시 에러가 나서, intent로 새 화면 연결해줌.
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        intent.putExtra("group", "group");
+                                        startActivity(intent);
+                                    })
+                                    .addOnFailureListener(e -> {
+                                        Log.w(TAG, "그룹 추가 과정에서 에러 발생", e);
+                                        Toast.makeText(GroupCreateActivity.this, "그룹생성 실패", Toast.LENGTH_SHORT).show();
+                                    });
                         }
-                    });
-            }
+                    }
+                });
         });
 
         //뒤로가기 버튼
@@ -132,6 +131,29 @@ public class GroupCreateActivity extends AppCompatActivity {
         Date date = new Date(now);
 
         return sdf.format(date);
+    }
+
+    //개인 시간표를 그룹 시간표에 넣는 함수
+    private void calculateGroupTimeTable(Map<String, List<Integer>> timetableMap) {
+        String curUserUid = auth.getCurrentUser().getUid();
+        FirebaseSchedules.getSchedules(
+                curUserUid,
+                dayNameList,
+                scheduleMap -> {
+                    scheduleMap.forEach((key, value) -> {
+                        List<Integer> groupTimeTable = CalculationUtil.change_ScheduleModel_ToIntArray(value);
+                        timetableMap.put(key, groupTimeTable);
+                    });
+                },
+                message -> {
+
+                }
+        );
+    }
+
+    private void addGroupTimeTable(String path, GroupTimeTableWrapper timetable) {
+        fireStore.collection("group_schedules").document(path)
+                .set(timetable);
     }
 }
 
--- app/src/main/java/com/example/woomansi/util/CalculationUtil.java
+++ app/src/main/java/com/example/woomansi/util/CalculationUtil.java
@@ -1,5 +1,43 @@
 package com.example.woomansi.util;
 
+import com.example.woomansi.data.model.ScheduleModel;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CalculationUtil {
 
+    //스케쥴 모듈을 불러와서 그룹 시간 int 배열을 갱신해주는 util
+    public static List<Integer> change_ScheduleModel_ToIntArray(List<ScheduleModel> schedules) {
+        int[] groupTimetable = new int[72];
+
+        schedules.forEach(scheduleModel ->
+        {
+            String startTime = scheduleModel.getStartTime();
+            String endTime = scheduleModel.getEndTime();
+
+            int startIndex = calculateIndex(startTime);
+            int endIndex = calculateIndex(endTime);
+            for (int i = startIndex; i <= endIndex; i++) {
+                groupTimetable[i]++;
+            }
+        });
+
+        List<Integer> result = Arrays.stream(groupTimetable)
+                .boxed()
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    private static int calculateIndex(String time) {
+        String[] array = time.split(":");
+        int hour =  Integer.parseInt(array[0]);
+        int minute = Integer.parseInt(array[1]);
+
+        hour = (hour - 6) * 4 ;
+        minute = (minute -1) / 15;
+
+        return hour + minute;
+    }
 }
