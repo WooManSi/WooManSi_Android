e80ed4661aab3e523ec03498c74658b9f958cb11
leejunha <jjuna0502@gmail.com>
2022-12-02 18:00:43 +0900

    fix : 레지스터 액티비티 intent 교ㅔ

--- app/src/main/java/com/example/woomansi/ui/screen/login/RegisterActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/login/RegisterActivity.java
@@ -149,7 +149,7 @@ public class RegisterActivity extends AppCompatActivity {
                                             }
                                         });
                                 Toast.makeText(RegisterActivity.this, "회원가입 완료", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
                                 startActivity(intent);
                             } else {
                                 Toast.makeText(RegisterActivity.this, "회원가입 실패", Toast.LENGTH_SHORT).show();
