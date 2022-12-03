c5c71112c720127c68660e582d5245edd0931acf
EunHee-21 <heh7878@gmail.com>
2022-12-04 05:32:05 +0900

    feat: 그룹 탈퇴 시 그룹시간표 데이터 변경 (1. 그룹장 탈퇴 시 그룹삭제 / 2. 그룹원 탈퇴 시 해당 그룹원 데이터를 제거)

--- app/src/main/java/com/example/woomansi/util/CalculationUtil.java
+++ app/src/main/java/com/example/woomansi/util/CalculationUtil.java
@@ -2,34 +2,9 @@ package com.example.woomansi.util;
 
 import com.example.woomansi.data.model.ScheduleModel;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CalculationUtil {
-
-    //스케쥴 모듈을 불러와서 그룹 시간 int 배열을 갱신해주는 util
-    public static List<Integer> change_ScheduleModel_ToIntArray(List<ScheduleModel> schedules) {
-        int[] groupTimetable = new int[72];
-
-        schedules.forEach(scheduleModel ->
-        {
-            String startTime = scheduleModel.getStartTime();
-            String endTime = scheduleModel.getEndTime();
-
-            int startIndex = calculateIndex(startTime);
-            int endIndex = calculateIndex(endTime);
-            for (int i = startIndex; i <= endIndex; i++) {
-                groupTimetable[i]++;
-            }
-        });
-
-        List<Integer> result = Arrays.stream(groupTimetable)
-                .boxed()
-                .collect(Collectors.toList());
-        return result;
-    }
 
     // origin에 scheduleModels의 값을 합치고 결과를 반환하는 함수
     public static List<Integer> unionLists(List<Integer> origin, List<ScheduleModel> models) {
@@ -40,6 +15,19 @@ public class CalculationUtil {
 
             for (int k = startIndex; k <= endIndex; k++)
                 origin.set(k, origin.get(k) + 1);
+        }
+        return origin;
+    }
+
+    // origin에 scheduleModels의 값을 빼고 결과를 반환하는 함수
+    public static List<Integer> minusLists(List<Integer> origin, List<ScheduleModel> models) {
+
+        for (int i = 0; i < models.size(); i++) {
+            int startIndex = calculateIndex(models.get(i).getStartTime());
+            int endIndex = calculateIndex(models.get(i).getEndTime());
+
+            for (int k = startIndex; k <= endIndex; k++)
+                origin.set(k, origin.get(k) - 1);
         }
         return origin;
     }
