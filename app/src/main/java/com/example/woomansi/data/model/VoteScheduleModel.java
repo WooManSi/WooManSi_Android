af8f8b94ad3a9a33aade4ea7d2dc8c06c3e5ff65
EunHee-21 <heh7878@gmail.com>
2022-12-04 16:06:27 +0900

    feat: 투표 관련 data Model class 임시 제작

---
+++ app/src/main/java/com/example/woomansi/data/model/VoteScheduleModel.java
@@ -0,0 +1,7 @@
+package com.example.woomansi.data.model;
+
+public class VoteScheduleModel {
+  private String startTime; //시작 시간 // format: "HH:mm" (예: "14:30")
+  private String endTime; //끝나는 시간
+  private int voteNum; //투표수
+}
