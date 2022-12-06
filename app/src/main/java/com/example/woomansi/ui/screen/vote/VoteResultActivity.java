44c196d10ed85d3b741690ce59b84fc0496e5b4d
EunHee-21 <heh7878@gmail.com>
2022-12-06 09:44:11 +0900

    feat: 투표가 종료될 시 서버에서 투표 결과를 가져오는 기능 구현

--- app/src/main/java/com/example/woomansi/ui/screen/vote/VoteResultActivity.java
+++ app/src/main/java/com/example/woomansi/ui/screen/vote/VoteResultActivity.java
@@ -3,13 +3,16 @@ package com.example.woomansi.ui.screen.vote;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.woomansi.R;
+import com.example.woomansi.data.model.GroupModel;
+import com.example.woomansi.data.model.VoteModel;
 import com.example.woomansi.data.model.VoteResultModel;
+import com.example.woomansi.data.model.VoteScheduleModel;
 import com.example.woomansi.ui.adapter.VoteResultListAdapter;
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.ArrayList;
 
@@ -19,13 +22,10 @@ public class VoteResultActivity extends AppCompatActivity {
   private ListView listView;
   private ArrayList<VoteResultModel> voteResultModelArrayList;
   private VoteResultListAdapter voteResultListAdapter;
-  MaterialToolbar topAppBar;
+  private MaterialToolbar topAppBar;
+  private GroupModel group;
 
-  private TextView dayOfWeek;
-  private TextView Time;
-
-  private FirebaseAuth auth;
-  private FirebaseFirestore fireStore;
+  private int maxVoteNum;
 
 
   @Override
@@ -35,6 +35,8 @@ public class VoteResultActivity extends AppCompatActivity {
 
     topAppBar= findViewById(R.id.voteResult_topAppBar);
     topAppBar.setNavigationOnClickListener(view -> finish());
+
+    group = (GroupModel) getIntent().getSerializableExtra("group");
 
     this.InitializeMemberData();
 
@@ -50,8 +52,60 @@ public class VoteResultActivity extends AppCompatActivity {
 
   public void InitializeMemberData()
   {
+    maxVoteNum = 0;
     voteResultModelArrayList = new ArrayList<>();
-    voteResultModelArrayList.add(new VoteResultModel("월", "14:00", "17:00"));
-    voteResultModelArrayList.add(new VoteResultModel("화", "14:00", "17:00"));
+
+    FirebaseFirestore
+        .getInstance()
+        .collection("groups")
+        .whereEqualTo("groupName", group.getGroupName())
+        .whereEqualTo("groupPassword", group.getGroupPassword())
+        .get()
+        .addOnCompleteListener(task -> {
+          //그룹을 찾았을 경우
+          if (task.isSuccessful()) {
+            DocumentSnapshot document = task.getResult().getDocuments().get(0);
+            DocumentReference ref = document.getReference();
+
+            FirebaseFirestore
+                .getInstance()
+                .collection("group_votes")
+                .document(ref.getId())
+                .get()
+                .addOnCompleteListener(task2 -> {
+                  if (task2.isSuccessful()) {
+                    DocumentSnapshot document2 = task2.getResult();
+
+                    //투표 존재 시
+                    if (document2.exists()) {
+                      VoteModel voteModel = document2.toObject(VoteModel.class);
+
+                      //1. 가장 큰 voteNum 값을 찾음
+                      voteModel.getVoteScheduleList()
+                          .forEach((key, value) -> {
+                            for (VoteScheduleModel voteScheduleModel : value) {
+                              if (maxVoteNum < voteScheduleModel.getVoteNum()) {
+                                maxVoteNum = voteScheduleModel.getVoteNum();
+                              }
+                            }
+                          });
+
+                      //2.maxVoteNum과 일치하는 결과들만 불러옴
+                      voteModel.getVoteScheduleList()
+                          .forEach((key, value) -> {
+                            for (VoteScheduleModel voteScheduleModel : value) {
+                              if (maxVoteNum == voteScheduleModel.getVoteNum()) {
+                                VoteResultModel voteResultModel = new VoteResultModel(key, voteScheduleModel.getStartTime(), voteScheduleModel.getEndTime());
+                                voteResultModelArrayList.add(voteResultModel);
+                              }
+                            }
+                          });
+                      //어뎁터에게 데이터 변경됬다는 알림 전송
+                      voteResultListAdapter.notifyDataSetChanged();
+                    }
+                  }
+                });
+          }
+        });
   }
 }
